c751892a5c6971f0f03d5445d4499dcd136148f5
Hamzarfs <hamzasarwar.rfs@gmail.com>
2025-01-08 15:32:05 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import ScrollToTop from './components/ScrollToTop';
 
-
-
-
-
 const App = () => {
     return (
         <Router>
--- src/components/Home/awardslogo.js
+++ src/components/Home/awardslogo.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect } from 'react';
+import Slider from 'react-slick';
 import '../../awards.css';
 import award1 from '../../images/award1.png';
 import award2 from '../../images/award2.png';
@@ -7,66 +7,71 @@ import award3 from '../../images/award3.png';
 import award4 from '../../images/award4.png';
 import award5 from '../../images/award5.png';
 import award6 from '../../images/award6.png';
-import 'swiper/css'; // Correct import for Swiper CSS in v8 and above
 
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const AwardsLogo = () => {
+
+  // Slick Slider settings for infinite scrolling
+  const settings = {
+    infinite: true, // Infinite loop
+    speed: 2000, // Speed of the transition
+    slidesToShow: 4, // Number of slides visible at once
+    slidesToScroll: 1, // Scroll 1 slide at a time
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 0, // Set autoplay speed to 0 for continuous movement
+    cssEase: 'linear', // Smooth transition effect
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="awards-logo-section py-3">
       <div className="container">
-        {/* Heading */}
-        {/* <div className="row text-center mb-4">
-          <div className="col-12">
-            <h2 className='choose-service-heading font-weight-bold py-3'>Award-Winning Video Production Company in USA</h2>
-          </div>
-        </div> */}
-
-        {/* Swiper for Logos */}
         <div className="row align-items-center">
           <div className="col-12">
-            <Swiper
-              spaceBetween={50}
-              slidesPerView={2}
-              breakpoints={{
-                640: {
-                  slidesPerView: 1,
-                },
-                768: {
-                  slidesPerView: 3,
-                },
-                1024: {
-                  slidesPerView: 5,
-                },
-              }}
-              loop={true}
-              autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-              }}
-            >
-              {/* Logo Slides */}
-              <SwiperSlide>
-                {/* <a target="_blank" href="https://www.designrush.com/agency/website-design-development/new-jersey"> */}
+            <Slider {...settings}>
+              <div>
                 <img src={award1} alt="Award 1" className="awards-logo-image img-fluid" />
-                {/* </a> */}
-              </SwiperSlide>
-              <SwiperSlide>
+              </div>
+              <div>
                 <img src={award2} alt="Award 2" className="awards-logo-image img-fluid" />
-              </SwiperSlide>
-              <SwiperSlide>
+              </div>
+              <div>
                 <img src={award3} alt="Award 3" className="awards-logo-image img-fluid" />
-              </SwiperSlide>
-              <SwiperSlide>
+              </div>
+              <div>
                 <img src={award4} alt="Award 4" className="awards-logo-image img-fluid" />
-              </SwiperSlide>
-              <SwiperSlide>
+              </div>
+              <div>
                 <img src={award5} alt="Award 5" className="awards-logo-image img-fluid" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src={award6} alt="Award 5" className="awards-logo-image img-fluid" />
-              </SwiperSlide>
-            </Swiper>
+              </div>
+              <div>
+                <img src={award6} alt="Award 6" className="awards-logo-image img-fluid" />
+              </div>
+            </Slider>
           </div>
         </div>
       </div>
